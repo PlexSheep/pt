341a64a0f517cd0a72a19ad1b6d967559dc05f54
Christoph J. Scherr <Christ.Scherr@newtec.de>
2023-09-20 18:37:06 +0200

    renaming things back to libpt, its kinda responsible

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+//! # A library for common needs
+//!
+//! `pt` aims to implement a number of functionalities that might me useful to develop
+//! programs in Rust. It aims to be a collection of generally, possibly useful things.
+//!
+//! `pt` is a project consisting of multiple smaller crates, all bundled together in this
+//! "main crate". Most crates will only show up if you activate their feature.
+
 #[cfg(feature = "core")]
 pub use pt_core     as core;
 #[cfg(feature = "bintols")]
