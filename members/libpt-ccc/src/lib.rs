9f393f0369aa79e9845c218b5cd0606c891ebbc1
PlexSheep <software@cscherr.de>
2024-01-15 23:28:01 +0100

    destruction of libpt-ccc

--- members/libpt-ccc/src/lib.rs
+++ members/libpt-ccc/src/lib.rs
@@ -19,14 +19,7 @@
 #![warn(clippy::pedantic)]
 
 //// IMPORTS ///////////////////////////////////////////////////////////////////////////////////////
-pub mod base;
-pub use base::{Error, Result, Value};
-pub mod term;
-pub use term::*;
-
-#[allow(unused_imports)]    // we possibly want to use all log levels
-use libpt_log::*;
-#[allow(unused_imports)]    // import more complex math stuff from there
+use libpt_log;
 use libpt_math;
 
 //// TYPES /////////////////////////////////////////////////////////////////////////////////////////
@@ -40,44 +33,8 @@ use libpt_math;
 //// ENUMS /////////////////////////////////////////////////////////////////////////////////////////
 
 //// STRUCTS ///////////////////////////////////////////////////////////////////////////////////////
-/// ## A Calculator struct
-///
-/// This struct does not do anything at the moment, but aims to be the target for high level
-/// control. Instead of using the [`Calculator`], you could just use the [`Term`] struct for
-/// lower level control.
-#[derive(Debug)]
-pub struct Calculator;
 
 //// IMPLEMENTATION ////////////////////////////////////////////////////////////////////////////////
-impl Calculator {
-    /// Do a single calculation without doing anything else
-    pub fn oneshot(t: String) -> Result<Value> {
-        trace!(orig=t, "parsing original string to Term");
-        let t = Term::new(t)?;
-        trace!("term has been parsed, starting Calculation");
-        debug!("parsed term: {t:#?}");
-        Self::calc(t)
-    }
-
-    /// ## Calculate a [`Term`]
-    ///
-    /// This method makes use of the
-    /// [shunting yard algorithm](https://en.wikipedia.org/wiki/Shunting_yard_algorithm) to
-    /// Calculate the final value of any term.
-    ///
-    /// This method only processes a single term at a time, without caching.
-    pub fn calc(mut t: Term) -> Result<Value> {
-        trace!("Calculating term {t:?}");
-        t.prepare()?;
-        t.process()?;
-        if t.result.is_none() {
-            let reason = format!("Term was processed but no result was assigned.");
-            // FIXME: unfitting error type
-            return Err(Error::SyntaxError(reason))
-        }
-        return t.result.unwrap()
-    }
-}
 
 //// PUBLIC FUNCTIONS //////////////////////////////////////////////////////////////////////////////
 
