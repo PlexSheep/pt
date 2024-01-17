4b7d08ae3703175a76eb402b77c22d10bd1cb7bc
PlexSheep <software@cscherr.de>
2024-01-17 17:01:38 +0100

    publish script mini update

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,7 +2,7 @@
 set -e
 cargo check --all-features
 cargo ws version --no-git-commit
-NEW_VERSION=$(cat Cargo.toml| rg '^\s*version\s*=\s*"([^"]*)"\s*$' -or '$1')
+NEW_VERSION=$(cat Cargo.toml | rg '^\s*version\s*=\s*"([^"]*)"\s*$' -or '$1')
 bash scripts/set_all_versions.sh $NEW_VERSION
 git add -A
 git commit -m "Release v$NEW_VERSION"
