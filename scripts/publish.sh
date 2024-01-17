54eac92ca7c9fa1edd8eea897538b68969a31c22
Christoph J. Scherr <Christoph.Scherr@newtec.de>
2024-01-17 16:52:32 +0100

    publish script cool

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-cargo ws publish --registry cscherr --publish-as-is
+set -e
+cargo check --all-features
+cargo ws version --no-git-commit
+NEW_VERSION=$(cat Cargo.toml| rg '^\s*version\s*=\s*"([^"]*)"\s*$' -or '$1')
+bash scripts/set_all_versions.sh $NEW_VERSION
+git add -A
+git commit -m "Release v$NEW_VERSION"
+cargo ws publish --registry cscherr --amend --publish-as-is
 cargo publish --registry cscherr -p libpt
