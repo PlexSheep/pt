4772893e8f79b683ec6a67c71d0c789e6f859bfe
PlexSheep <software@cscherr.de>
2024-01-18 22:02:06 +0100

    release script improvement

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,11 +4,10 @@ NEW_VERSION=$(cat Cargo.toml | rg '^\s*version\s*=\s*"([^"]*)"\s*$' -or '$1')
 VERSION=$(git rev-parse HEAD)
 GIT_COMMIT_SHA=$(git rev-parse HEAD)
 BODY="
-
-$(git log $(git describe --tags --abbrev=0)..HEAD --pretty="- %s")
+$(git log $(git describe --tags --abbrev=0)..HEAD --pretty="- %s" -oneline --decorate)
 "
 USER=PlexSheep
-git tag "v$NEW_VERSION" || echo "could not tag"
+git tag "v$NEW_VERSION-test" || echo "could not tag"
 curl -X 'POST' \
 	'https://git.cscherr.de/api/v1/repos/PlexSheep/pt/releases' \
 	-H 'accept: application/json' \
@@ -16,8 +15,8 @@ curl -X 'POST' \
 	-H 'Content-Type: application/json' \
 	-d '{
   "body": "'"$BODY"'",
-  "draft": true,
-  "name": "string",
+  "draft": false,
+  "name": "v'$NEW_VERSION'",
   "prerelease": true,
   "tag_name": "v'$NEW_VERSION'",
   "target_commitish": "'$GIT_COMMIT_SHA'"
