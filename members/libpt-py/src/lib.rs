c69d116c7584308c3fbc80c2286e0ab9f701f5d6
PlexSheep <software@cscherr.de>
2024-02-28 22:06:55 +0100

    fix py feature

--- members/libpt-py/src/lib.rs
+++ members/libpt-py/src/lib.rs
@@ -8,7 +8,7 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 
 /// A Python module implemented in Rust.
 #[pymodule]
-#[pyo3(name="libpt")]
+#[pyo3(name = "libpt")]
 fn libpt_py(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
