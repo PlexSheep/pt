7150f0aefd05e450908333309c53c65e0c658762
PlexSheep <software@cscherr.de>
2024-03-01 21:21:34 +0100

    Release v0.4.0-alpha.1

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,13 +2,8 @@
 set -e
 cargo check --all-features
 echo ">>>>>>>> SELECT A NEW VERSION"
-cargo ws version --no-git-commit
+cargo ws version
 NEW_VERSION=$(cat Cargo.toml | rg '^\s*version\s*=\s*"([^"]*)"\s*$' -or '$1')
-bash scripts/set_all_versions.sh $NEW_VERSION
-git add -A
-git commit -m "Release v$NEW_VERSION" || (echo ">>>>>>>> COMMIT FAILED OR THERE WAS NOTHING TO COMMIT"; sleep 5)
-echo ">>>>>>>> SKIP!!!!!"
-cargo ws version --amend
 echo ">>>>>>>> PUBLISHING RELEASE FOR REPO"
 bash scripts/release.sh
 echo ">>>>>>>> PUBLISHING TO CRATES.IO NEXT"
@@ -16,14 +11,10 @@ sleep 10
 cargo publish -p libpt-log
 cargo publish -p libpt-core
 cargo publish -p libpt-bintols
-cargo publish -p libpt-math
-cargo publish -p libpt-net
 cargo publish -p libpt
 echo ">>>>>>>> PUBLISHING TO CSCHERR.DE NEXT"
 sleep 3
 cargo publish --registry cscherr -p libpt-log
 cargo publish --registry cscherr -p libpt-core
 cargo publish --registry cscherr -p libpt-bintols
-cargo publish --registry cscherr -p libpt-math
-cargo publish --registry cscherr -p libpt-net
 cargo publish --registry cscherr -p libpt
