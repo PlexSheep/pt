b06a821f2931bf3cfd7f85bc0945541849a4af47
PlexSheep <software@cscherr.de>
2024-07-09 19:43:43 +0200

    docs(cli): add crate level documentation
    but I'm lazy

--- members/libpt-cli/src/lib.rs
+++ members/libpt-cli/src/lib.rs
@@ -1,3 +1,5 @@
+//! This module bundles a lot of good CLI tools, and adds some of it's own, to make development of
+//! CLI apps easier and more ergonomic.
 #![warn(clippy::pedantic, clippy::style, clippy::nursery)]
 pub mod args;
 pub mod printing;
